bfd323f48324c9c1348335bfeedb7d0cb562c43d
Max Base <MaxBaseCode@Gmail.Com>
2025-01-10 15:48:21 +0100

    Update info.php

--- info.php
+++ info.php
@@ -2,8 +2,9 @@
 <html lang="en-US" dir="ltr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Info</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow, noarchive, nosnippet">
     <style>
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
